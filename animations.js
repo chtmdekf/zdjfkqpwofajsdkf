099ee5625286e9c13886966526b31f1a58838780
chtmdekf <64016037+chtmdekf@users.noreply.github.com>
2022-03-06 23:58:24 +0900

    Create animations.js

---
+++ animations.js
@@ -0,0 +1,105 @@
+(function (_0x25c537, _0x15e010) {
+    var _0x109382 = _0x3e8b, _0x249e50 = _0x25c537();
+    while (!![]) {
+        try {
+            var _0x5994a8 = parseInt(_0x109382('0x85')) / 0x1 * (parseInt(_0x109382('0x7f')) / 0x2) + -parseInt(_0x109382('0x89')) / 0x3 * (parseInt(_0x109382('0x8a')) / 0x4) + -parseInt(_0x109382('0x84')) / 0x5 * (parseInt(_0x109382('0x86')) / 0x6) + -parseInt(_0x109382('0x88')) / 0x7 + -parseInt(_0x109382('0x83')) / 0x8 * (parseInt(_0x109382('0x81')) / 0x9) + -parseInt(_0x109382('0x82')) / 0xa * (parseInt(_0x109382('0x80')) / 0xb) + parseInt(_0x109382('0x87')) / 0xc;
+            if (_0x5994a8 === _0x15e010)
+                break;
+            else
+                _0x249e50['push'](_0x249e50['shift']());
+        } catch (_0xc4a714) {
+            _0x249e50['push'](_0x249e50['shift']());
+        }
+    }
+}(_0x11dc, 0x5bc93));
+var _0x101cf5 = (function () {
+        var _0x37362c = !![];
+        return function (_0x1b106f, _0x3cb564) {
+            var _0x2e2667 = _0x37362c ? function () {
+                if (_0x3cb564) {
+                    var _0x562e92 = _0x3cb564['a' + 'p' + 'p' + 'l' + 'y'](_0x1b106f, arguments);
+                    return _0x3cb564 = null, _0x562e92;
+                }
+            } : function () {
+            };
+            return _0x37362c = ![], _0x2e2667;
+        };
+    }()), _0x38bc83 = _0x101cf5(this, function () {
+        var _0x44d325 = function () {
+                var _0x4b5171;
+                try {
+                    _0x4b5171 = Function('r' + 'e' + 't' + 'u' + 'r' + 'n' + '\x20' + '(' + 'f' + 'u' + 'n' + 'c' + 't' + 'i' + 'o' + 'n' + '(' + ')' + '\x20' + ('{' + '}' + '.' + 'c' + 'o' + 'n' + 's' + 't' + 'r' + 'u' + 'c' + 't' + 'o' + 'r' + '(' + '\x22' + 'r' + 'e' + 't' + 'u' + 'r' + 'n' + '\x20' + 't' + 'h' + 'i' + 's' + '\x22' + ')' + '(' + '\x20' + ')') + (')' + ';'))();
+                } catch (_0x55873c) {
+                    _0x4b5171 = window;
+                }
+                return _0x4b5171;
+            }, _0x15f6e1 = _0x44d325(), _0x41c432 = _0x15f6e1['c' + 'o' + 'n' + 's' + 'o' + 'l' + 'e'] = _0x15f6e1['c' + 'o' + 'n' + 's' + 'o' + 'l' + 'e'] || {}, _0xdaf9a = [
+                'l' + 'o' + 'g',
+                'w' + 'a' + 'r' + 'n',
+                'i' + 'n' + 'f' + 'o',
+                'e' + 'r' + 'r' + 'o' + 'r',
+                'e' + 'x' + 'c' + 'e' + 'p' + 't' + 'i' + 'o' + 'n',
+                't' + 'a' + 'b' + 'l' + 'e',
+                't' + 'r' + 'a' + 'c' + 'e'
+            ];
+        for (var _0x459b95 = 0x0; _0x459b95 < _0xdaf9a['l' + 'e' + 'n' + 'g' + 't' + 'h']; _0x459b95++) {
+            var _0x20e9e7 = _0x101cf5['c' + 'o' + 'n' + 's' + 't' + 'r' + 'u' + 'c' + 't' + 'o' + 'r']['p' + 'r' + 'o' + 't' + 'o' + 't' + 'y' + 'p' + 'e']['b' + 'i' + 'n' + 'd'](_0x101cf5), _0x440e86 = _0xdaf9a[_0x459b95], _0x128cbf = _0x41c432[_0x440e86] || _0x20e9e7;
+            _0x20e9e7['_' + '_' + 'p' + 'r' + 'o' + 't' + 'o' + '_' + '_'] = _0x101cf5['b' + 'i' + 'n' + 'd'](_0x101cf5), _0x20e9e7['t' + 'o' + 'S' + 't' + 'r' + 'i' + 'n' + 'g'] = _0x128cbf['t' + 'o' + 'S' + 't' + 'r' + 'i' + 'n' + 'g']['b' + 'i' + 'n' + 'd'](_0x128cbf), _0x41c432[_0x440e86] = _0x20e9e7;
+        }
+    });
+_0x38bc83();
+var visi_price = ![];
+function price_() {
+    if (visi_price == ![])
+        document['q' + 'u' + 'e' + 'r' + 'y' + 'S' + 'e' + 'l' + 'e' + 'c' + 't' + 'o' + 'r']('b' + 'o' + 'd' + 'y' + '\x20' + '>' + '\x20' + 'd' + 'i' + 'v' + '.' + 'm' + 'i' + 'd' + 'd' + 'l' + 'e' + '2')['s' + 't' + 'y' + 'l' + 'e']['o' + 'p' + 'a' + 'c' + 'i' + 't' + 'y'] = 0x1, document['q' + 'u' + 'e' + 'r' + 'y' + 'S' + 'e' + 'l' + 'e' + 'c' + 't' + 'o' + 'r']('b' + 'o' + 'd' + 'y' + '\x20' + '>' + '\x20' + 'd' + 'i' + 'v' + '.' + 'm' + 'i' + 'd' + 'd' + 'l' + 'e')['s' + 't' + 'y' + 'l' + 'e']['d' + 'i' + 's' + 'p' + 'l' + 'a' + 'y'] = 'n' + 'o' + 'n' + 'e', document['q' + 'u' + 'e' + 'r' + 'y' + 'S' + 'e' + 'l' + 'e' + 'c' + 't' + 'o' + 'r']('b' + 'o' + 'd' + 'y' + '\x20' + '>' + '\x20' + 'd' + 'i' + 'v' + '.' + 'm' + 'i' + 'd' + 'd' + 'l' + 'e' + '2')['s' + 't' + 'y' + 'l' + 'e']['d' + 'i' + 's' + 'p' + 'l' + 'a' + 'y'] = 'b' + 'l' + 'o' + 'c' + 'k', visi_price = !![];
+    else
+        visi_price == !![] ? (document['q' + 'u' + 'e' + 'r' + 'y' + 'S' + 'e' + 'l' + 'e' + 'c' + 't' + 'o' + 'r']('b' + 'o' + 'd' + 'y' + '\x20' + '>' + '\x20' + 'd' + 'i' + 'v' + '.' + 'm' + 'i' + 'd' + 'd' + 'l' + 'e')['s' + 't' + 'y' + 'l' + 'e']['d' + 'i' + 's' + 'p' + 'l' + 'a' + 'y'] = 'b' + 'l' + 'o' + 'c' + 'k', document['q' + 'u' + 'e' + 'r' + 'y' + 'S' + 'e' + 'l' + 'e' + 'c' + 't' + 'o' + 'r']('b' + 'o' + 'd' + 'y' + '\x20' + '>' + '\x20' + 'd' + 'i' + 'v' + '.' + 'm' + 'i' + 'd' + 'd' + 'l' + 'e' + '2')['s' + 't' + 'y' + 'l' + 'e']['d' + 'i' + 's' + 'p' + 'l' + 'a' + 'y'] = 'n' + 'o' + 'n' + 'e', visi_price = ![]) : alert('?');
+}
+function z_x_s() {
+    visi_price == !![] && (fadeOutEffect_close('b' + 'o' + 'd' + 'y' + '\x20' + '>' + '\x20' + 'd' + 'i' + 'v' + '.' + 'm' + 'i' + 'd' + 'd' + 'l' + 'e' + '2'), visi_price = ![]);
+}
+function _0x11dc() {
+    var _0x15e244 = [
+        '2052438FvsEGt',
+        '20753724HyPNke',
+        '4378101xABSDH',
+        '87834ikkpBf',
+        '32MOJwqi',
+        '458572eZGxGP',
+        '1165076nKewhn',
+        '198zocJSx',
+        '10ZhNtLF',
+        '183432iNySYL',
+        '5nhEGmU',
+        '2nGaTIE'
+    ];
+    _0x11dc = function () {
+        return _0x15e244;
+    };
+    return _0x11dc();
+}
+function fadeIn_() {
+    var _0x4788b8 = 0.5, _0x534bf3 = 0x5, _0x569246 = 0x0, _0x188364 = 0x1, _0x962829 = setInterval(function () {
+            _0x569246 >= _0x188364 && (_0x569246 = _0x188364, clearInterval(_0x962829)), document['q' + 'u' + 'e' + 'r' + 'y' + 'S' + 'e' + 'l' + 'e' + 'c' + 't' + 'o' + 'r']('b' + 'o' + 'd' + 'y' + '\x20' + '>' + '\x20' + 'd' + 'i' + 'v' + '.' + 'm' + 'i' + 'd' + 'd' + 'l' + 'e')['s' + 't' + 'y' + 'l' + 'e']['o' + 'p' + 'a' + 'c' + 'i' + 't' + 'y'] = _0x569246, _0x569246 += _0x188364 / (0x3e8 / _0x534bf3 * _0x4788b8);
+        }, _0x534bf3);
+}
+function _0x3e8b(_0x450e78, _0x175c42) {
+    var _0x4d85f5 = _0x11dc();
+    return _0x3e8b = function (_0x38bc83, _0x101cf5) {
+        _0x38bc83 = _0x38bc83 - 0x7f;
+        var _0x57fe08 = _0x4d85f5[_0x38bc83];
+        return _0x57fe08;
+    }, _0x3e8b(_0x450e78, _0x175c42);
+}
+function fadeIn() {
+    document['q' + 'u' + 'e' + 'r' + 'y' + 'S' + 'e' + 'l' + 'e' + 'c' + 't' + 'o' + 'r']('b' + 'o' + 'd' + 'y' + '\x20' + '>' + '\x20' + 'd' + 'i' + 'v' + '.' + 'm' + 'i' + 'd' + 'd' + 'l' + 'e')['s' + 't' + 'y' + 'l' + 'e']['o' + 'p' + 'a' + 'c' + 'i' + 't' + 'y'] = 0x0;
+    var _0x39ba71 = setInterval(function () {
+        var _0x10d417 = document['q' + 'u' + 'e' + 'r' + 'y' + 'S' + 'e' + 'l' + 'e' + 'c' + 't' + 'o' + 'r']('b' + 'o' + 'd' + 'y' + '\x20' + '>' + '\x20' + 'd' + 'i' + 'v' + '.' + 'm' + 'i' + 'd' + 'd' + 'l' + 'e');
+        !_0x10d417['s' + 't' + 'y' + 'l' + 'e']['o' + 'p' + 'a' + 'c' + 'i' + 't' + 'y'] && (_0x10d417['s' + 't' + 'y' + 'l' + 'e']['o' + 'p' + 'a' + 'c' + 'i' + 't' + 'y'] = 0x0), _0x10d417['s' + 't' + 'y' + 'l' + 'e']['o' + 'p' + 'a' + 'c' + 'i' + 't' + 'y'] < 0x1 ? _0x10d417['s' + 't' + 'y' + 'l' + 'e']['o' + 'p' + 'a' + 'c' + 'i' + 't' + 'y'] = _0x10d417['s' + 't' + 'y' + 'l' + 'e']['o' + 'p' + 'a' + 'c' + 'i' + 't' + 'y'] + 0.09 : (console['l' + 'o' + 'g']('e' + 'n' + 'd'), clearInterval(_0x39ba71));
+    }, 0xa);
+}
+function fadeOutEffect_close(_0x14738e) {
+    var _0x49952b = document['q' + 'u' + 'e' + 'r' + 'y' + 'S' + 'e' + 'l' + 'e' + 'c' + 't' + 'o' + 'r'](_0x14738e + ''), _0x418b06 = setInterval(function () {
+            !_0x49952b['s' + 't' + 'y' + 'l' + 'e']['o' + 'p' + 'a' + 'c' + 'i' + 't' + 'y'] && (_0x49952b['s' + 't' + 'y' + 'l' + 'e']['o' + 'p' + 'a' + 'c' + 'i' + 't' + 'y'] = 0x1), _0x49952b['s' + 't' + 'y' + 'l' + 'e']['o' + 'p' + 'a' + 'c' + 'i' + 't' + 'y'] > 0x0 ? _0x49952b['s' + 't' + 'y' + 'l' + 'e']['o' + 'p' + 'a' + 'c' + 'i' + 't' + 'y'] -= 0.09 : (document['q' + 'u' + 'e' + 'r' + 'y' + 'S' + 'e' + 'l' + 'e' + 'c' + 't' + 'o' + 'r']('b' + 'o' + 'd' + 'y' + '\x20' + '>' + '\x20' + 'd' + 'i' + 'v' + '.' + 'm' + 'i' + 'd' + 'd' + 'l' + 'e')['s' + 't' + 'y' + 'l' + 'e']['o' + 'p' + 'a' + 'c' + 'i' + 't' + 'y'] = 0x0, document['q' + 'u' + 'e' + 'r' + 'y' + 'S' + 'e' + 'l' + 'e' + 'c' + 't' + 'o' + 'r']('b' + 'o' + 'd' + 'y' + '\x20' + '>' + '\x20' + 'd' + 'i' + 'v' + '.' + 'm' + 'i' + 'd' + 'd' + 'l' + 'e')['s' + 't' + 'y' + 'l' + 'e']['d' + 'i' + 's' + 'p' + 'l' + 'a' + 'y'] = 'b' + 'l' + 'o' + 'c' + 'k', document['q' + 'u' + 'e' + 'r' + 'y' + 'S' + 'e' + 'l' + 'e' + 'c' + 't' + 'o' + 'r']('b' + 'o' + 'd' + 'y' + '\x20' + '>' + '\x20' + 'd' + 'i' + 'v' + '.' + 'm' + 'i' + 'd' + 'd' + 'l' + 'e' + '2')['s' + 't' + 'y' + 'l' + 'e']['d' + 'i' + 's' + 'p' + 'l' + 'a' + 'y'] = 'n' + 'o' + 'n' + 'e', console['l' + 'o' + 'g']('e' + 'n' + 'd'), fadeIn_(), clearInterval(_0x418b06));
+        }, 0xa);
+}
